8eacb03c41304c431f50fcced314e6995569e8a9
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-24 14:33:11 +0900

    Change Screen by Orientation

--- app/src/main/java/com/gardenlab/sampleorientation/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampleorientation/MainActivity.java
@@ -1,17 +1,41 @@
 package com.gardenlab.sampleorientation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private String name;
+    private EditText editText;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showToast("onCreate() called");
+
+        editText = findViewById(R.id.editText);
+        editText.setPadding(20,0,0,0);
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = editText.getText().toString();
+                showToast("Saved in Variable");
+            }
+        });
+
+        if(savedInstanceState!=null) {
+            name = savedInstanceState.getString("name");
+            showToast("Recover value : " + name);
+        }
+
     }
 
     @Override
@@ -32,7 +56,14 @@ public class MainActivity extends AppCompatActivity {
         showToast("onDestroy() called");
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("name", name); // name변수의 값 저장
+    }
+
     public void showToast(String data) {
-        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
     }
 }
